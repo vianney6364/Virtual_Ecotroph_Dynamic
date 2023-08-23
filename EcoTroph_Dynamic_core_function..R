49c20564c8501ec3b7299ae03352bf2903868582
viavia6364 <vianney.guibourddeluzinais@agrocampus-ouest.fr>
2023-08-23 23:38:08 +0200

    updates on Script comments and user manual explanations

--- EcoTroph_Dynamic_core_function..R
+++ EcoTroph_Dynamic_core_function..R
@@ -14,7 +14,7 @@
 # i.e TE= exp((-2.162) + (-0.025)*sst + b + a*sst)*1.038013 with a and b depending of ecosystem type
 
 
-#Load required library:
+#'*Load required library:*
 # install.packages("Rcpp")
 # install.packages("tidyverse")
 # install.packages("data.table")
@@ -22,66 +22,68 @@ library(Rcpp)
 library(tidyverse)
 library(data.table)
 
-# load several fonction 
-source("FUNCTIONS/production_tl_2to3.R") #process to calculate consumer production between TL=2 and TL=3
-source("FUNCTIONS/geom_sequence_prod.R") # geometric sequence to speed up the process to calculate consumer production
-# ---------------------------------------------------------------------
+#'*load several fonctions* 
 source("FUNCTIONS/transfer_efficiency.R") # calulculate TE HTL
 source("FUNCTIONS/dyn_reference state_add_rect_morta.R") # calculate reference state of EcoTroph Dynamic
-sourceCpp("rcpp function add morta/tl_by_dtime_bis2_rect_morta.cpp") #Compute the lower and upper bounds for each trophic level within the food web
+sourceCpp("FUNCTIONS/tl_by_dtime_bis2_rect_morta.cpp") #Compute the lower and upper bounds for each trophic level within the food web
 
-sourceCpp("rcpp function add morta/dyn_dispatch_prod_rect_morta.cpp") # production redistribution proportionally to the width of the old classes included into each of the new ones 
-sourceCpp("rcpp function add morta/compute_new_time_rcpp_rect_morta_a_fixed.cpp") #compute the new (t+1) state of the ecosystem
-sourceCpp("rcpp function add morta/phi_pass_ct1_rcpp_rect_morta.cpp") #make biomass flow to the next trophic class and compute new production value
-sourceCpp("rcpp function add morta/add_morta_biologic.cpp") # Marine heatwave mortality law
+sourceCpp("FUNCTIONS/dyn_dispatch_prod_rect_morta.cpp") # production redistribution proportionally to the width of the old classes included into each of the new ones 
+sourceCpp("FUNCTIONS/compute_new_time_rcpp_rect_morta_a_fixed.cpp") #compute the new (t+1) state of the ecosystem
+sourceCpp("FUNCTIONS/phi_pass_ct1_rcpp_rect_morta.cpp") #make biomass flow to the next trophic class and compute new production value
+sourceCpp("FUNCTIONS/add_morta_biologic.cpp") # Marine heatwave mortality law
 
 # ---------------------------------------------------------------------
 # 3 examples: 3 theoritical grid cells representing polar, temperate, and tropical, respectively, for 14 year (see at the end of the script)
 # Each example is based on the average value of SST from 1982 to 2011 : NOAA _ AVHRR data  https://www.ncei.noaa.gov/access/metadata/landing-page/bin/iso?id=gov.noaa.ncdc:C00680
-load("DATA/example_data.RData")
 # ---------------------------------------------------------------------
 
-#' #'*to test EcoTroph Dynamic function execute le 4 next row*
+#' #'*to test EcoTroph Dynamic function execute create_data_envi() function*
 # ---------------------------------------------------------------------
-# data_envi : data frame with 6 columns:
-# time_step (number of the fortnight from 0 to 364 as simulated 14 year)
-# 2.npp (npp in mole of Carbon .m-2.s-1)
-# 3.sst (in °C)
-# 4.mhw (booleen either TRUE or FALSE at each fortnight)
-# 5.eco_type : 3 choices of ecosystem types: "polar","temperate","tropical"# the sensivty of te_htl to temperature is varying among ecosystem types (du pontavice et al., 2019 DOI: https://doi.org/10.1111/gcb.14944)
+#'*1. create virtual environment function*
+#d=duration of simulated MHW with d as integer value between 1 and 10 (between 1 to 10 time_step i.e between a fortnight and 5 months duration)
+#i=intensity of simulated MHW seq(1,16,0.5)) with i positive numeric  value between 1 and 16 (between 1 to 16 degree above normal SST)
+#biome=which biome you want to simulate with biome chara ("polar","temperate","tropical")
+#h=percentage of biomass not supporting simulated MHW. With h positive numeric  value between 0 and 1 (between 0 and 100% species acclimatisation capacity)
 
-# nb_time_step=366;dtime=1/24;eco_type="tropical";data_envi=data_envi_tropical;ratio=0.8
-
-#-------------------------------------------
-#'*1. create virtual environment data_frame*
-#d=duration of simulated MHW
-#i=intensity of simulated MHW
-#h=percentage of biomass not supporting simulated MHW
-create_data_envi<-function (d,biome,h){
-  if (biome == "polar"){pp=4.499573e-07;clim=0;surface_area=7035490465} #surface_area: Surface of the the random theoritical cell in m²
-  if (biome == "temperate"){pp=5.898543e-07;clim=12;surface_area=7556565706}
-  if (biome == "tropical"){pp=1.320424e-06;clim=25;surface_area=12286884909}
+create_data_envi<-function (d,i,biome,h){
+  if (biome == "polar"){npp=4.499573e-07;clim=0;surface_area=7035490465} #surface_area: Surface of  random  cell in m² Bering Sea (coordinates: 173.5°W, 55.5°N)
+  if (biome == "temperate"){npp=5.898543e-07;clim=12;surface_area=7556565706} #surface_area: Surface of  random  cell in m² West of Ireland (coordinates: 12.5°W, 52.5°N)
+  if (biome == "tropical"){npp=1.320424e-06;clim=25;surface_area=12286884909} #surface_area: Surface of  random  cell in m² Southwest of Galápagos (coordinates: 94.5°W, 3.5°S)
   
-  return(data.frame(eco_type=rep(biome,366),
-                    time_step=seq(1,366,1),
-                    npp=rep((pp*365/26 * 24 * 3600 * 9 * 12 * surface_area * 10^(-6)),366), #divided by 26 to get NPP per fornight
-                    sst=c(rep(clim,5),rep(clim+i,d),rep(clim,360-d+1)),
-                    mhw=c(rep(F,5),rep(T,d),rep(F,360-d+1)),
-                    ratio=h))
+  data_envi<-data.frame(eco_type=rep(biome,366),
+                        time_step=seq(1,366,1),
+                        # NPP: Net primary production in t.an-1 (data in mole.m-2.s-1 --> x 12 g.mol-1 (mole of carbon to g of carbon) 
+                        #  x 365*24*3600 (day*hour*sec) (second ==> year)
+                        # x * 9 (carbon ==> biomass)
+                        npp=rep((npp*365/26 * 24 * 3600 * 9 * 12 * surface_area * 10^(-6)),366), #divided by 26 to get NPP per fornight
+                        sst=c(rep(clim,5),rep(clim+i,d),rep(clim,360-d+1)),
+                        mhw=c(rep(F,5),rep(T,d),rep(F,360-d+1)),
+                        ratio=h)
+  return(data_envi)
   
   
 }
+
+#'* data_envi : data frame with 6 columns:*
+# time_step (number of the fortnight from 0 to 364 as simulated 14 year)
+# 2.npp (npp in mole of Carbon .m-2.s-1) fix npp over time and estimate within each cell as the average over 1950_2100 with GFDL-ESM2M (Dunne et al., 2012; DOI: https://doi.org/10.1175/JCLI-D-11-00560.1) and the emission scenario : RCP8.5 ("no mitigation policy" scenario)
+# 3.sst (in °C)
+# 4.mhw (booleen either TRUE or FALSE at each fortnight)
+# 5.biome : 3 choices of ecosystem types: "polar","temperate","tropical"# the sensivty of te_htl to temperature is varying among ecosystem types (du pontavice et al., 2019 DOI: https://doi.org/10.1111/gcb.14944)
+# 6. ratio: percentage of biomass not supporting simulated MHW. With h positive numeric  value between 0 and 1 (between 0 and 100% species acclimatisation capacity)
+
 #----------------------------------------------------------------------------------
 
-EcoTroph_dyn_iterative_MHW<-function(data_envi,nb_time_step,dtime){ 
+ecotroph_dynamic_core<-function(data_envi,nb_time_step,dtime){ 
   #'*1.Reference state*
+  #'*1.1.environnemental condition for reference state*
   npp_ref<-data_envi$npp[1]
   sst_ref<-data_envi$sst[1]
   mhw_ref<-data_envi$mhw[1]
   eco_type=data_envi$eco_type[1]
   ratio=data_envi$ratio[1]
   
-  #'* 2. Creation of unequal trophic classes length for same time step to reach the upper trophic classe*
+  #'* 1.2. Computation of MHW associated mortality if MHW==T*
   if (mhw_ref==F){
     
     morta_ref<-0  
@@ -89,30 +91,27 @@ EcoTroph_dyn_iterative_MHW<-function(data_envi,nb_time_step,dtime){
     morta_ref<-morta_bio_rcpp(eco_type=eco_type,sst=sst_ref)*ratio
     
   }
-  #Compute unequal trophic classes division
+  #'* 1.3. Compute unequal trophic classes limit of reference state* 
   CT_dataframe_ref_v4_rect_morta<-rcpp_tl_by_dtime2_rect_morta(sst=sst_ref,dtime=dtime,morta=morta_ref)
-  # # CT_dataframe_ref2<-tl_by_dtime2(sst_ref,dtime)
-  # CT_dataframe_ref_v4_morta<-CT_dataframe_ref_v4_morta %>% mutate(TLinf=lag(tl),TLsup=tl,time=round(time,3))
-  # CT_dataframe_ref_v4_morta$TLinf[1]<-2.000 
+
   
-  #'*3. Compute production and biomass for each trophic class*
-  dyn_eco_ref_fishing_v4_rect_morta<-ecotroph_core_dyn_ref_rect_morta(CT_dataframe_ref_v4_rect_morta,eco_type,dtime,npp_ref,sst_ref,mhw_ref,morta=morta_ref)
+  #'*1.4. Compute production and biomass for each trophic class*
+  ET_ref_rect_morta<-ecotroph_core_dyn_ref_rect_morta(CT_dataframe_ref_v4_rect_morta,eco_type,dtime,npp_ref,sst_ref,mhw_ref,morta=morta_ref) %>%mutate(time_step=0) 
   
   #------------------------------------------------------
   #'*STEP T to T+1* 
   #----------------------------------------
-  ET_ref_rect_morta<-dyn_eco_ref_fishing_v4_rect_morta%>%mutate(time_step=0) 
-  
   result_rect_morta<-list()   # ceration of a list to store all the fortnight output
   
   result_rect_morta[[paste('time_step',0)]]<-ET_ref_rect_morta #save reference state in the list
-  #envi data prepation for all the time step computation
+  #envi data prepation for all the time step computation remove initial condition [-1]
   sst<-data_envi$sst[-1] 
   npp<-data_envi$npp[-1]
   mhw<-data_envi$mhw[-1]
   
-  #'*4. computing next time_step*
-  for (i in 1:(nb_time_step-1)){
+  #'*2. computing next time_step*
+  #'* 2.1. for each time_step i,computation of MHW associated mortality if MHW==T*
+    for (i in 1:(nb_time_step-1)){
     
     if (mhw[i]==F){
       
@@ -121,7 +120,7 @@ EcoTroph_dyn_iterative_MHW<-function(data_envi,nb_time_step,dtime){
       mortality<-morta_bio_rcpp(eco_type=eco_type,sst=sst[i])*ratio
       mortality2<-mortality
       ssts<-sst[i]
-      ET_ref_rect_morta<-ET_ref_rect_morta %>% mutate(sst=ssts,
+      ET_ref_rect_morta<-ET_ref_rect_morta %>% mutate(sst=ssts,  #transition between reference state and new environnemental conditions
                                                       te=te_funct(eco_type,sst),
                                                       mortality=mortality2,
                                                       MHW=mhw[i],
@@ -131,30 +130,21 @@ EcoTroph_dyn_iterative_MHW<-function(data_envi,nb_time_step,dtime){
                                                       heat_tho=(kinetic*mortality),
                                                       kinetic_heat=kinetic+heat_tho*dtime)
     }
-    #'*4.1 creating offset of phi and compute new production value*
+    #'*2.2. make biomass flow to the next trophic class and compute new production value*
     phi_pass_ct1_v4_rect_morta<-phi_pass_ct1_rcpp_rect_morta(ET=select(ET_ref_rect_morta,-production))
-    # phi_pass_ct1_v4_morta<-ET%>%mutate(flow_actualize=lag(flow_tl_2to7)*exp(-(-log(te)+fishing/kinetic)*delta_tl_dyn),
-    #                           flow_m_actualize=flow_actualize*(1-exp(-(-log(te)+fishing/kinetic)*delta_tl_dyn))/
-    #                             (delta_tl_dyn*(-log(te)+fishing/kinetic)),
-    #                           production=flow_m_actualize*delta_tl_dyn)%>%
-    #   as.data.frame()
-    # print(head(phi_pass_ct1_v4_rect_morta))
+
     
-    #'* 4.2 Compute limit of Trophic classes time t+1 :need to pick new time sst and npp*
-    #Compute unequal trophic classes division at time t+1
-    # dtime<-1/26 # possible to change value to see influence on biomass flow through the food web
+    #'* 2.3. Compute limit of Trophic classes time t+1 *
     CT_dataframe_t1_v4_rect_morta<-rcpp_tl_by_dtime2_rect_morta(sst=sst_ref,dtime=dtime,
                                                                 morta=mortality)
-    #'*4.3 actualisation of phi by CT*
+    #'*2.4. actualisation of phi by CT*
     actualize_t1_rect_morta<-dyn_dispatch_prod_rect_morta_rcpp(ET_T1=phi_pass_ct1_v4_rect_morta,ET_T2=CT_dataframe_t1_v4_rect_morta)
-    # print(head(actualize_t1_rect_morta))
-    # dim(actualize_t1)
-    
-    #'*4.4 compute t+1 te,kinetic,Production and biomass *
+
+    #'*2.5. compute t+1 te,kinetic,Production and biomass of time t+1: compute the new (t+1) state of the ecosystem*
     test_t1_rect_morta<-compute_new_time_rcpp_rect_morta_a_fixed(actualize_t1=actualize_t1_rect_morta,eco_type=eco_type,sst_ref=sst_ref,
                                                                  nppi=npp[i],ssti=sst[i],time_stepi=i,dtime=dtime,mhwi=mhw[i],morta=mortality)
     
-    result_rect_morta[[paste('time_step',i)]]<-test_t1_rect_morta #add each new time_temp to the list
+    result_rect_morta[[paste('time_step',i)]]<-test_t1_rect_morta #add each time_temp (fortnigtht) to the list
     
     ET_ref_rect_morta<-test_t1_rect_morta # new time_step becomes reference state for computing the next one
   }
@@ -162,10 +152,44 @@ EcoTroph_dyn_iterative_MHW<-function(data_envi,nb_time_step,dtime){
     mutate(intensity=data_envi$intensity[1], #adding simulation in information to compare then after
            duration=data_envi$duration[1],
            ratio=data_envi$ratio[1]) 
+
+#'*2.6. this bloc is not mandatory: it just format data by trophic class of 0.1 width from tl =2 to tl=5.5* 
+#'*you can put it in comment to see the raw output data*
+  # result_rect_morta<-result_rect_morta %>%
+  #   mutate(tl=round(tl,1)) %>%
+  #   group_by(time_step,tl) %>%
+  #       summarise(biomass=sum(biomass),
+  #             production=sum(production),
+  #             te=mean(te),
+  #             kin=mean(kinetic),
+  #             phi=mean(flow_m_per_tl),
+  #             kin_heat=mean(kinetic_heat),
+  #             mortality=mean(mortality),
+  #             heat_tho=mean(heat_tho), #with heat_tho=(kinetic*mortality)
+  #             mhw=mhw[1])
   return(result_rect_morta) #return the result for one theoritical ocean cell
 }
 
 
 #' *to test function*
-#' output_ecotroph_dyn_iterative_temperate2<-EcoTroph_dyn_iterative("entre le num d'iteration que tu veux i.e 1500",1/24,"temperate",data_envi)
-#'profvis::profvis(output_ecotroph_dyn_iterative_temperate2<-EcoTroph_dyn_iterative(10000,1/24,"temperate",data_envi))
+# ---------------------------------------------------------------------
+# 3 EXAMPLES FOR POLAR, TEMPERATE, TROPICAL  
+# 3 example with random parameter that you can choose
+#With:
+#d=duration of simulated MHW with d as integer value between 1 and 10 (between 1 to 10 time_step i.e between a fortnight and 5 months duration)
+#i=intensity of simulated MHW seq(1,16,0.5)) with i positive numeric  value between 1 and 16 (between 1 to 16 degree above normal SST)
+#biome=which biome you want to simulate with biome chara ("polar","temperate","tropical")
+#h=percentage of biomass not supporting simulated MHW. With h positive numeric  value between 0 and 1 (between 0 and 100% species acclimatisation capacity)
+
+# POLAR
+data_envi<-create_data_envi(d=5,i=2.5,biome = "polar",h=0.5) #choose your parameter here 
+output_ecotroph<-ecotroph_dynamic_core(data_envi,nb_time_step = 366,dtime = 1/26)
+# TEMPERATE
+data_envi<-create_data_envi(d=5,i=2.5,biome = "temperate",h=0.5) #choose your parameter here 
+output_ecotroph<-ecotroph_dynamic_core(data_envi,nb_time_step = 366,dtime = 1/26)
+# TROPICAL
+data_envi<-create_data_envi(d=5,i=2.5,biome = "tropical",h=0.5) #choose your parameter here 
+output_ecotroph<-ecotroph_dynamic_core(data_envi,nb_time_step = 366,dtime = 1/26)
+# ---------------------------------------------------------------------
+head(output_ecotroph)
+dim(output_ecotroph)
--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 
 R scripts to run the EcoTroph model in a theoretical unexploited ecosystem and analyze climate change effects
 
-WARNING: In this repository, you will find an original implementation of the EcoTroph modelling framework (Gascuel 2005, Gascuel et al. 2011), which is completely independent of the EcoTroph Package (https://cran.r-project.org/web/packages/EcoTroph/index.html). 
+WARNING: In this repository, you will find an original implementation of the EcoTroph_Dynamic modelling framework (Gascuel et al. 2008), which is completely independent of the EcoTroph Package (https://cran.r-project.org/web/packages/EcoTroph/index.html). 
 
-EcoTroph is a simplified semi-empirical model of the flow of biomass through the food web, from primary producers to top predators (Gascuel 2005, Gascuel et al. 2011). It has been used in particulat to assess the ecosytem impact of fishing. In this implementation of EcoTroph, biomass and production of upper trophic levels (at each trophic level) are calculated based on the trophic transfer efficiency and the flow kinetic at each trophic level, using sea water temperature, net primary production and transfer efficiency of high trophic levels (see du Pontavice et al 2021 https://doi.org/10.1111/gcb.15576) as the model inputs. 
+EcoTroph is a simplified semi-empirical model of the flow of biomass through the food web, from primary producers to top predators (Gascuel 2005, Gascuel et al. 2011). It has been used in particulat to assess the ecosytem impact of fishing.
+In this implementation of EcoTroph, biomass and production of upper trophic levels (at each trophic level) are calculated based on the trophic transfer efficiency and the flow kinetic at each trophic level, using sea water temperature, net primary production and transfer efficiency of high trophic levels (see du Pontavice et al 2021 https://doi.org/10.1111/gcb.15576) as the model inputs. 
 
 The details and the theoretical basis about the implementation of EcoTroph _Dynamic available in this repository would be presented in Guibourd de luzinaiset al. (2023).
 
 How to run those scripts?
-1. Open the R script: EcoTroph_Dynamic_core_function.R. 
-2. Run the first lines and the fonction called "ecotroph_core". 
-3. Try to use ecotroph with the examples at the end of the script (4 examples) 
-  or use your own data following the details in the R script.
+  1. Open the R script: EcoTroph_Dynamic_core_function.R. 
+  2. Run the first lines and the fonctions called "create_data_envi",and "ecotroph_dynamic_core". 
+3. Try to use ecotroph with the examples at the end of the script (3 examples) 
+or use your own data following the details in the R script.
 
 REFERENCES
 Guibourd de Luzinais, V., Gascuel, D., Reygondeau, G., Stock, C., D., & Cheung, W. W. L. (2021). Large potential impacts of Marine Heatwave on ecosystem functioning. manuscript submitted for publication in Global Change Biology. 
